2b70d6bf9bdfc4c79b531e6da9af15d2f9373c4e
Tony Won <tonywon89@gmail.com>
2016-02-02 13:56:11 -0500

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,15 @@
+class TagsController < ApplicationController
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    Tag.find(params[:id]).destroy
+    redirect_to tags_path
+  end
+end
