26be88b6eba62bc9c8234a4bdb2b4635f72a7224
Tony Won <tonywon89@gmail.com>
2016-02-03 19:01:30 -0500

    Sorcery authentiation complete

--- Gemfile
+++ Gemfile
@@ -32,7 +32,11 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+# For attachments
 gem "paperclip"
+
+# Authentication library
+gem 'sorcery'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   include ArticlesHelper
 
+  before_filter :require_login, only: [:new, :create, :edit, :update, :destroy]
+
   def index
     @articles= Article.all
   end
